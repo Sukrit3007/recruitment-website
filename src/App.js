5db6a7022d9026c34a1e76e63527043f31cdc31a
Sukrit <sukrit3007@outlook.com>
2023-07-31 17:51:41 +0530

    Secondary hero page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './Pages/Home/Home';
+import SecondPage from './Pages/SecondPage/SecondPage';
 
 function App() {
   return (
     <>
       <Home/>
+      <SecondPage/>
     </>
   );
 }
---
+++ src/Pages/SecondPage/SecondPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './SecondPage.css'
+
+
+const SecondPage = () => {
+  return (
+    <>
+    <div className="hero2-content">
+        <div className="hero2-text">
+            <h1 className="hero2-heading">We connect young professionals to <br/>internships and  early career opportunities.</h1>
+            <div className="hero2-buttons">
+            <p>We understand that starting a career can be daunting, which is why we strive to simplify the <br/>process and ensure a smooth transition into the professional world.</p>
+            <button className="hero2-button"><a href="https://www.youtube.com/">Watch Video </a></button>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default SecondPage
